cfe2d589c289bf72ad0357792301f144b90d0755
ajitatif <gokalp.gurbuzer@yandex.com>
2017-11-22 13:51:16 +0300

    tests start

--- angular/ng-workshop/src/app/app.component.spec.ts
+++ angular/ng-workshop/src/app/app.component.spec.ts
@@ -1,11 +1,20 @@
 import { TestBed, async } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { APP_BASE_HREF } from '@angular/common';
+
 import { AppComponent } from './app.component';
+import { WelcomeHeadingComponent } from './welcome-heading/welcome-heading.component';
+import { ServiceConfigComponent } from './service-config/service-config.component';
+import { CasePipe } from './case.pipe';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ FormsModule, AppRoutingModule ],
       declarations: [
-        AppComponent
+        AppComponent, WelcomeHeadingComponent, ServiceConfigComponent, CasePipe
       ],
+      providers: [ { provide: APP_BASE_HREF, useValue: '/'} ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -13,15 +22,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it(`should have as title 'Gökalp\'in Evi'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.appTitle).toEqual('Gökalp\'in Evi');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h1').textContent).toContain('gökalp\'in evi');
   }));
 });
