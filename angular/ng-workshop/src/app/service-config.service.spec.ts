08c47b2ea721444c5cf1e656e61c78ee1514339e
ajitatif <gokalp.gurbuzer@yandex.com>
2017-11-22 14:52:37 +0300

    service testing

--- angular/ng-workshop/src/app/service-config.service.spec.ts
+++ angular/ng-workshop/src/app/service-config.service.spec.ts
@@ -1,15 +1,37 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject, async } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { ServiceConfig } from './model/service-config';
 import { ServiceConfigService } from './service-config.service';
 
 describe('ServiceConfigService', () => {
-  beforeEach(() => {
+	let service: ServiceConfigService;
+    let httpMock: HttpTestingController;
+
+    beforeEach(() => {
+	
     TestBed.configureTestingModule({
-      providers: [ServiceConfigService]
+      imports: [ HttpClientTestingModule ],
+      providers: [ ServiceConfigService ]
     });
+
+
+	httpMock = TestBed.get(HttpTestingController);
+    service = TestBed.get(ServiceConfigService);
+
   });
 
-  it('should be created', inject([ServiceConfigService], (service: ServiceConfigService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('should return Test Config', (done) => {
+  	service.getAllServices().subscribe(result =>  {
+  		expect(result[0].name).toBe('Test Config');
+  		done();
+  	});
+
+  	let request = httpMock.expectOne('http://localhost:8080/admin/');
+  	request.flush( [{ name: 'Test Config'}] );
+
+  	httpMock.verify();
+  });
 });
