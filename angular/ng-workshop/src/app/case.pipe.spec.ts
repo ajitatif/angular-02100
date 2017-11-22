b0c1a05cbfdf90b64325599454b0a36d03950bb4
ajitatif <gokalp.gurbuzer@yandex.com>
2017-11-22 12:11:14 +0300

    Revert "angular"
    This reverts commit fdbc12feacb491f5cd1038d510f5def8b458fe98.

--- angular/ng-workshop/src/app/case.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CasePipe } from './case.pipe';
-
-describe('CasePipe', () => {
-  it('create an instance', () => {
-    const pipe = new CasePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- angular/ng-workshop/src/app/model/service-config.ts
+++
@@ -1,8 +0,0 @@
-export class ServiceConfig {
-	public id: number;
-	public name: string;
-	public type: string;
-	public value: string;
-	public active: boolean;
-	public applicationName: string;
-}
