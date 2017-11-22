6179b1477d22467e5dfe29627dbf9e1bd7ba8676
ajitatif <gokalp.gurbuzer@yandex.com>
2017-11-22 12:10:45 +0300

    agai

--- angular/ng-workshop/src/app/service-config.service.ts
+++ angular/ng-workshop/src/app/service-config.service.ts
@@ -1,20 +1,20 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { catchError } from 'rxjs/operators';
 
 import { ServiceConfig } from './model/service-config';
 
+const httpOptions = {
+headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+  
 @Injectable()
 export class ServiceConfigService {
 
   private serviceUrl: string = 'http://localhost:8080/admin/';
 
   constructor(private httpClient: HttpClient) {  }
-
-  const httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
-  };
   
   public getAllServices(): Observable<ServiceConfig[]> {
   	return this.httpClient.get<ServiceConfig[]>(this.serviceUrl).pipe(catchError((err, observable) => { console.log(err); return observable; }));
